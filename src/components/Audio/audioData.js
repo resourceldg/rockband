fbbac912ecb8f5807983b1a9ad83e8dde8059dc5
lucas <resourceldg@gmail.com>
2021-02-26 13:33:48 -0300

    add full mp3 static

--- src/components/Audio/audioData.js
+++ src/components/Audio/audioData.js
@@ -1,51 +1,51 @@
 export const audioData = [
 	{
 		name: "Siempre libre siempre vivo - Paso a paso",
-		url: "DERRIBAR.mp3",
+		url: "PASO A PASO.mp3",
 	},
 	{
 		name: "Siempre libre siempre vivo - Skarnaval",
-		url: "https://drive.google.com/uc?id=1B0ws8rezXXi6-Pymz4YHsD2vJr_O9xO2&export=download",
+		url: "SKARNAVAL.mp3",
 	},
 	{
 		name: "Siempre libre siempre vivo - El lobo espera",
-		url: "https://drive.google.com/uc?id=13C4cNDBgWbIYwd6XFNbg0n9eDYu6q49y&export=download",
+		url: "EL LOBO ESPERA.mp3",
 	},
 	{
 		name: "Siempre libre siempre vivo - Canta pajarito",
-		url: "https://drive.google.com/uc?id=16PkTpNVoh87-L-fc97MfiQ5y5RE30d5q&export=download",
+		url: "CANTA PAJARITO.mp3|",
 	},
 	{
 		name: "Siempre libre siempre vivo - Ahora no nos frena el tiempo",
-		url: "https://drive.google.com/uc?id=1yelPu3tWehzCUnAXMv_vK5-GMpZxs1O6&export=download",
+		url: "AHORA NO NOS FRENA EL TIEMPO.mp3",
 	},
 	{
 		name: "Sentir musiquero - Derribar",
-		url: "https://drive.google.com/uc?id=1TDTGHXcBS7uiTLbLEAuou3Ws5q22qQ3U&export=download",
+		url: "DERRIBAR.mp3",
 	},
 	{
 		name: "Sentir musiquero - Salis",
-		url: "https://drive.google.com/uc?id=1kjYQiFAQ49aI5syDv1g1CH4EnuBPRZEP&export=download",
+		url: "SALIS.mp3",
 	},
 	{
 		name: "Sentir musiquero - El tiempo en libertad",
-		url: "https://drive.google.com/uc?id=1C-urW-BGpViXST9GvdfV2cKmdZ_z20KT&export=download",
+		url: "EL TIEMPO EN LIBERTAD.mp3",
 	},
 	{
 		name: "Sentir musiquero - Canción de la caldela",
-		url: "https://drive.google.com/uc?id=16heAtV1LuEWBJrFUUaEx2TU2R6vVMk_2&export=download",
+		url: "CANCIÓN DE LA CALDELA.mp3",
 	},
 	    
 	{   
 		name: "Sentir musiquero - Grito, aire de chacarera",
-		url: "https://drive.google.com/uc?id=1ubC5hUMmpqHjKhXMUxsr0mxZVsBP9Z-Y&export=download",
+		url: "GRITO AIRE DE CHACARERA.mp3",
 	},
 	{
 		name: "Sentir musiquero - MonoSka",
-		url: " https://drive.google.com/uc?id=1kou97m4kmt0qGxRIe6cwUT1lrdFHZFwa&export=download",
+		url: "MONOSKA.mp3",
 	},
 	{
 		name: "Sentir musiquero - Esté quien esté",
-		url: "https://drive.google.com/uc?id=1xmmipPXroxVNG6FvszQIp39pZxSlORhY&export=download",
+		url: "ESTE QUIEN ESTE.mp3",
 	}
 ]
