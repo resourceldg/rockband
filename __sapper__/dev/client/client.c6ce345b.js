3d511e617b252ff422e7571353b2a4014ef5bded
lucas <resourceldg@gmail.com>
2021-02-26 20:52:21 -0300

    add download

--- __sapper__/dev/client/client.c6ce345b.js
+++
@@ -1,2 +0,0 @@
-import './client.838f1cbb.js';
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpZW50LmM2Y2UzNDViLmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
--- src/routes/playroom.svelte
+++ src/routes/playroom.svelte
@@ -2,16 +2,18 @@
 	import Audio from './../components/Audio/Audio.svelte';
  
 </script>
-<h3>Escuchar trucks...</h3>
+<h3 class="text-xl">Llevate nuestra música a donde quieras...</h3>
 <div class="grid grid-cols-1">
     <div class="m-30">
         <Audio/>
     </div>
-    
-       <div class="grid md:grid-cols-4 gap-4 rounded-bl place-items-center  shadow-lg p-4 mt-30 mb-10">
-        <div class="grid justify-self-start" ><img src="disc.jpg " alt="disc" width="70" height="70"></div>
+        <div class="grid md:grid-cols-4 gap-4 rounded-bl place-items-center  shadow-lg p-4 mt-30 mb-10">
+        <div class="grid justify-self-start" >
+            <img src="desc.svg " alt="disc" width="70" height="70"></div>
         <div class="grid col-span-2 justify-self-center">  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum</div>
-        <div class="grid sm:justify-self-end"><button class="border-yellow-500 border-2"> > </button></div>
+        <div class="grid sm:justify-self-end"> <a href="PASO A PASO.mp3" download="PASO A PASO.mp3">
+           download
+          </a></div>
     </div>
     
 </div>
